5628698dae6092f7968f7a7d9b5ccbe6c04f522a
Jason Bell <ja50n8@yahoo.co.uk>
2017-06-15 21:06:02 +0100

    Pokedex
    Added PokeCell

---
+++ Pokedex2/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex2
+//
+//  Created by Jason Bell on 15/06/2017.
+//  Copyright © 2017 Cold Entertainment. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
