33bc5684b829212df088aeb878d278f5a57767d3
Jason Bell <ja50n8@yahoo.co.uk>
2017-06-18 01:47:54 +0100

    Pokedex
    Alamofire & JSON

---
+++ Pokedex2/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex2
+//
+//  Created by Jason Bell on 18/06/2017.
+//  Copyright © 2017 Cold Entertainment. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co/"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
